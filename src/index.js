01264aa410645895530684eb5590f6f08060b3aa
Isaac Araujo <isaac_maciel@hotmail.com>
2020-07-02 17:14:48 -0300

    Projeto Calculadora Finalizado

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./main/Calculator";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1> Calculadora </h1>
+    <Calculator />
+
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
